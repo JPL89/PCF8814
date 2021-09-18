2514ac3c29bdf5f71e3be8e235321976c74b718b
Josimar Pereira <josimarpereiraleite@gmail.com>
2021-09-18 13:56:07 -0300

    Add files via upload

---
+++ Fonts/Font_5x7.h
@@ -0,0 +1,21 @@
+/*
+ * Font_5x7.h
+ *
+ * Created: 18/09/2021 11:57:27
+ *  Author: Desktop
+ */ 
+
+
+#ifndef FONT_5X7_H_
+#define FONT_5X7_H_
+
+#include <avr/pgmspace.h>
+
+const unsigned char font_5x8[] PROGMEM =
+{
+	5, 8, 1, 32, 0,
+	
+};
+
+
+#endif /* FONT_5X7_H_ */
